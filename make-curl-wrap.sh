52e9a37b2925fa2fc0ec8b693ee1edbfcc609a8f
Burt Holzman <burt@fnal.gov>
2016-02-10 10:06:34 -0600

    Set correct filename and mode for curl wrapper

--- make-curl-wrap.sh
+++ make-curl-wrap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cat curl-wrap-template-pre.txt curl.py curl-wrap-template-post.txt > test.sh
+cat curl-wrap-template-pre.txt curl.py curl-wrap-template-post.txt > curl-wrap.sh
+chmod +x curl-wrap.sh
+
